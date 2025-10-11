dace1f1ce6d916f482a2b1de311841672aba720a
ShashankDL-2025 <dlshashank0@gmail.com>
2025-10-11 08:06:14 +0530

    updated

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setuptools.setup(
     name='audio-speech-to-sign-language-converter',
     version='0.1.0',
     description='Python project',
-    author='Jigar Gajjar',
-    author_email='jigargajjar.55jg@gmail.com',
-    url='https://github.com/jigargajjar55/Audio-Speech-To-Sign-Language-Converter',
+    author='Shashank D L',
+    author_email='dlshashank0@gmail.com@gmail.com',
+    url='https://github.com/ShashankDL-2025/Audio-Speech-To-Sign-Language-Converter',
     packages=setuptools.find_packages(),
     setup_requires=['nltk', 'joblib','click','regex','sqlparse','setuptools'],
-)
+ )
